e0ed8d5f2c080bc01299bdb89a777e1af13eb423
GentaArya <mgentaarya@yahoo.com>
2024-08-26 11:49:56 +0700

    fix crash

--- src/router/Ruangan/RuanganRoutes.js
+++ src/router/Ruangan/RuanganRoutes.js
@@ -3,7 +3,6 @@ import { addRuangan } from "../../controllers/Ruangan/PostRuanganController.js";
 import { GetAllRuangan, GetSingleRuangan} from "../../controllers/Ruangan/GetRuanganController.js";
 import { updateNamaRuangan } from "../../controllers/Ruangan/PutRuanganController.js";
 import { DeleteRuangan } from "../../controllers/Ruangan/DeleteRuanganController.js";
-
 const RuanganRouter = express.Router();
 RuanganRouter.post("/ruangan", addRuangan);
 RuanganRouter.get("/ruangan", GetAllRuangan);
