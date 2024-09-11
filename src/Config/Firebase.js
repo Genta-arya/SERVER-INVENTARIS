802f864891cb7860a5ef1343d0945426d8c34460
GentaArya <mgentaarya@yahoo.com>
2024-09-11 09:09:12 +0700

    add report barang keluar

--- src/Config/Firebase.js
+++ src/Config/Firebase.js
@@ -2,21 +2,22 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import multer from 'multer';
 
-// Konfigurasi Firebase
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAVAa1n3KFbokVqoxgbhm8qZKXAxChfoxA",
-  authDomain: "inventaris-15fde.firebaseapp.com",
-  projectId: "inventaris-15fde",
-  storageBucket: "inventaris-15fde.appspot.com",
-  messagingSenderId: "666060624790",
-  appId: "1:666060624790:web:1176b5fff172d2881bc3b7",
-  measurementId: "G-WNZWRN960Z"
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
 };
 
-// Inisialisasi Firebase
-const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app); // Ekspor storage dari Firebase
 
-// Konfigurasi Multer untuk menyimpan file di memori
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app); 
+
+
 const memoryStorage = multer.memoryStorage();
-export const upload = multer({ storage: memoryStorage }); // Ekspor upload dari Multer
+export const upload = multer({ storage: memoryStorage });
