75ba5657251fdb102bd75cbccfec60d2521414e7
M.Gentha arya pratama <74033154+Genta-arya@users.noreply.github.com>
2026-02-02 10:29:37 +0700

    Update Uploads.js

--- src/Config/Uploads.js
+++ src/Config/Uploads.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const UPLOAD_URL = "https://cloud.mystorages.my.id/uploads.php";
+const UPLOAD_URL = "https://cloud.apiservices.my.id/uploads.php";
 export const uploadProfile = async (data) => {
   try {
     const response = await axios.post(UPLOAD_URL, data, {
